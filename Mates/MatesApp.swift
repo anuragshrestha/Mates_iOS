5c02366b9081822e11403a800410e1d36413ccab
Anurag Shrestha <anuragshrestha16@gmail.com>
2025-04-17 20:03:33 -0600

    Initial commit for Mates_iOS project

---
+++ Mates/MatesApp.swift
@@ -0,0 +1,20 @@
+//
+//  MatesApp.swift
+//  Mates
+//
+//  Created by Anurag Shrestha on 4/17/25.
+//
+
+import SwiftUI
+
+@main
+struct MatesApp: App {
+    let persistenceController = PersistenceController.shared
+
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+                .environment(\.managedObjectContext, persistenceController.container.viewContext)
+        }
+    }
+}
