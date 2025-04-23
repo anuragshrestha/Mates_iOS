85d300f036e4a5cb7ae2699aa1f09b4fe74f8ddf
Anurag Shrestha <anuragshrestha16@gmail.com>
2025-04-23 00:01:39 -0600

    Button to select the option

--- Mates/UICommon/DropDownField.swift
+++ Mates/UICommon/DropDownField.swift
@@ -5,4 +5,29 @@
 //  Created by Anurag Shrestha on 4/19/25.
 //
 
-import Foundation
+
+import SwiftUI
+
+
+struct DropdownField: View {
+    var title: String
+    var value: String
+    var action: () -> Void
+    
+    var body: some View {
+        Button(action: action) {
+            HStack {
+                Text(value.isEmpty ? title : value)
+                    .foregroundColor(value.isEmpty ? .gray : .white)
+                    .font(.customfont(.semibold, fontSize: 18))
+                Spacer()
+                Image(systemName: "chevron.down")
+                    .foregroundColor(.gray)
+            }
+            .padding()
+            .background(RoundedRectangle(cornerRadius: 10).fill(Color.white.opacity(0.1)))
+            
+        }
+        .padding(.vertical, 15)
+    }
+}
