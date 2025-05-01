111b61a795ee07ac48b8c7f175b59acf0b288bb6
Anurag Shrestha <anuragshrestha16@gmail.com>
2025-04-30 22:33:28 -0600

    confirm signup user interface

--- Mates/View/AuthenticationView/ConfirmSignUpView.swift
+++ Mates/View/AuthenticationView/ConfirmSignUpView.swift
@@ -8,11 +8,48 @@
 import SwiftUI
 
 struct ConfirmSignUpView: View {
+    
+    
+    @StateObject var confirmVM = ConfirmSignUpViewModel()
+    @State var showAlert: Bool = false
+    @State var alertMessage:String = ""
+    
     var body: some View {
-        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+        
+        VStack {
+            InputField(text: $confirmVM.confirmationCode, placeholder: "Enter confirmation code")
+            
+            
+            NavigationLink(destination: MainView(), isActive: $confirmVM.isConfirmed) {
+                CustomButton(title: "Confirm") {
+                    print("pressed confirm button")
+                    //checks if the code is correct
+                    if !confirmVM.confirmationCode.isEmpty {
+                        confirmVM.isConfirmed = true
+                    } else{
+                        showAlert = true
+                        alertMessage = "Please enter a valid code"
+                    }
+                }
+                .alert("Missing Code", isPresented: $showAlert){
+                    Button("Ok", role: .cancel) {}
+                } message: {
+                    Text(alertMessage)
+                }
+                .padding(.horizontal, 20)
+                .padding(.vertical, 10)
+            }
+        }
+        .frame(maxWidth: .infinity, maxHeight: .infinity)
+        .background(Color.black)
+        .ignoresSafeArea()
+      
     }
+    
 }
 
 #Preview {
-    ConfirmSignUpView()
+    NavigationView{
+        ConfirmSignUpView()
+    }
 }
