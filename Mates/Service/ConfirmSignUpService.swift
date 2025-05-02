8ba573b94aa7904d2084d72e77719eed7ed1ff32
Anurag Shrestha <anuragshrestha16@gmail.com>
2025-05-01 23:43:44 -0600

    post api for confirm sign up

---
+++ Mates/Service/ConfirmSignUpService.swift
@@ -0,0 +1,46 @@
+//
+//  ConfirmSignUpService.swift
+//  Mates
+//
+//  Created by Anurag Shrestha on 5/1/25.
+//
+
+import Foundation
+
+struct ConfirmRequest: Codable {
+    let username:String
+    let confirmationCode: String
+}
+
+struct ConfirmResponse: Codable{
+    let success:Bool
+    let message:String?
+    let error: String?
+}
+
+class ConfirmSignUpService{
+    
+    static let shared = ConfirmSignUpService()
+    private init(){}
+    
+    func confirmSignUp(data: ConfirmRequest)async throws -> ConfirmResponse{
+        
+        //checks if the url is a valid url
+        guard let url = URL(string: "http://10.0.0.225:4000/confirm-Signup") else {
+            throw URLError(.badURL)
+        }
+        
+        //creates a HTTP request
+        var request = URLRequest(url: url)
+        request.httpMethod = "POST"
+        request.setValue("application/json", forHTTPHeaderField: "Content-Type")
+        
+        request.httpBody = try JSONEncoder().encode(data)
+        
+        let (responseData, _) = try await URLSession.shared.data(for: request)
+        
+        let response = try JSONDecoder().decode(ConfirmResponse.self, from: responseData)
+        return response
+        
+    }
+}
