2db74a874393ec9958404f988951dcbf758ae0db
Anurag Shrestha <anuragshrestha16@gmail.com>
2025-05-02 19:04:04 -0600

    signIn function to implement the SignInService and return success and message

--- Mates/ViewModel/AuthViewModel/SignInViewModel.swift
+++ Mates/ViewModel/AuthViewModel/SignInViewModel.swift
@@ -11,4 +11,24 @@ class SignInViewModel: ObservableObject {
     @Published var email: String = ""
     @Published var password: String = ""
     @Published var isSecure: Bool = false
+    @Published var isSignedIn:Bool = false
+    
+    func signIn(completion: @escaping (Bool, String?) -> Void) {
+        
+        let request = SignInRequest(
+           username: email,
+           password: password
+        )
+        
+        Task{
+            do{
+                let response = try await SignInService.shared.signInService(data: request)
+                completion(response.success, response.message ?? response.error)
+            }catch{
+                completion(false, error.localizedDescription)
+            }
+        }
+    }
 }
+
+
