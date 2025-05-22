7e0717f2f74e446706ab481bcbd27a80cc069b96
Anurag Shrestha <anuragshrestha16@gmail.com>
2025-05-21 22:35:51 -0600

    api call for confirm forgot password

--- Mates/Service/ForgotPasswordService.swift
+++ Mates/Service/ForgotPasswordService.swift
@@ -17,16 +17,32 @@ struct ForgotResponse: Codable{
     let error: String?
 }
 
+
+struct ConfirmForgotRequest: Codable{
+    let username: String
+    let confirmationCode: String
+    let password:String
+}
+
+
+struct ConfirmForgotResponse: Codable{
+    let success:Bool
+    let message: String?
+    let error:String?
+}
+
 class ForgotPasswordService {
     
     static let shared = ForgotPasswordService()
     private init(){}
     
     
+    
+    //Makes API call for the forgot password
     func handleForgotPassword(data: ForgotRequest) async throws -> ForgotResponse {
         
         //checks if the url is a valid url
-        guard let url = URL(string: "http://localhost:4000/forgot-password") else {
+        guard let url = URL(string: "http://10.0.0.225:4000/forgot-password") else {
             throw URLError(.badURL)
         }
         
@@ -41,5 +57,26 @@ class ForgotPasswordService {
         let response = try JSONDecoder().decode(ForgotResponse.self, from: responseData)
         
         return response
+    }
+    
+    
+    //Makes API call for confirm forgot password
+    func handleConfirmForgotPassword(data: ConfirmForgotRequest) async throws -> ConfirmForgotResponse {
+        
+        guard let url = URL(string: "http://10.0.0.225:4000/confirm-forgot-password") else {
+            throw URLError(.badURL)
+        }
+        
+        var request = URLRequest(url: url)
+        request.httpMethod = "POST"
+        request.setValue("application/json", forHTTPHeaderField: "Content-Type")
+        
+        request.httpBody = try JSONEncoder().encode(data)
+        
+        let (response, _) = try await URLSession.shared.data(for: request)
+        
+        let decodeResponse = try JSONDecoder().decode(ConfirmForgotResponse.self, from: response)
+        
+        return decodeResponse
     }
 }
