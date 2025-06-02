71e551e05a2837a8cc9ff6a3d31770e2d12d6ddf
Anurag Shrestha <anuragshrestha16@gmail.com>
2025-06-02 00:02:45 -0600

    changed the json data into multipart form data for handling image

--- Mates/Service/SignUpService.swift
+++ Mates/Service/SignUpService.swift
@@ -6,14 +6,16 @@
 //
 
 import Foundation
+import _PhotosUI_SwiftUI
 
-struct SignUpRequest: Codable{
+struct SignUpRequest {
     let university_name: String
     let major: String
     let school_year: String
     let first_name: String
     let last_name: String
     let username: String
+    let image: UIImage
     let password: String
 }
 
@@ -24,6 +26,16 @@ struct SignUpResponse: Codable{
     let error: String?
 }
     
+
+extension Data {
+    mutating func append(_ string: String) {
+        if let data = string.data(using: .utf8) {
+            append(data)
+        }
+    }
+}
+
+
 
 class SignUpService{
     static let shared = SignUpService()
@@ -39,10 +51,12 @@ class SignUpService{
         //creates a HTTP request
         var request = URLRequest(url: url)
         request.httpMethod = "POST"
-        request.setValue("application/json", forHTTPHeaderField: "Content-Type")
         
-       
-        request.httpBody = try JSONEncoder().encode(data)
+        let boundary = UUID().uuidString
+        request.setValue("multipart/form-data; boundary=\(boundary)", forHTTPHeaderField: "Content-Type")
+ 
+        let body = createMultipartBody(data: data, boundary: boundary)
+        request.httpBody = body
         
         let (responseData, _) = try await URLSession.shared.data(for: request)
         
@@ -50,4 +64,43 @@ class SignUpService{
         return decodedResponse
 
     }
+    
+    
+    private func createMultipartBody(data: SignUpRequest, boundary: String) -> Data {
+        
+     
+        
+        var body = Data()
+
+        let parameters: [String: String] = [
+            "university_name": data.university_name,
+            "major": data.major,
+            "school_year": data.school_year,
+            "first_name": data.first_name,
+            "last_name": data.last_name,
+            "username": data.username,
+            "password": data.password
+        ]
+
+        for (key, value) in parameters {
+            body.append("--\(boundary)\r\n")
+            body.append("Content-Disposition: form-data; name=\"\(key)\"\r\n\r\n")
+            body.append("\(value)\r\n")
+        }
+
+        // Add image
+        if let imageData = data.image.jpegData(compressionQuality: 0.8) {
+            let filename = "profile.jpg"
+            body.append("--\(boundary)\r\n")
+            body.append("Content-Disposition: form-data; name=\"image\"; filename=\"\(filename)\"\r\n")
+            body.append("Content-Type: image/jpeg\r\n\r\n")
+            body.append(imageData)
+            body.append("\r\n")
+        }
+
+        body.append("--\(boundary)--\r\n")
+        return body
+    }
+    
+
 }
