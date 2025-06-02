f9101ab28cc24f0fd0bd8458ef64321ebcb65977
Anurag Shrestha <anuragshrestha16@gmail.com>
2025-06-02 00:04:20 -0600

    view mdoel for post that checks if the post was created successfully or not

--- Mates/ViewModel/Post/PostViewModel.swift
+++ Mates/ViewModel/Post/PostViewModel.swift
@@ -6,8 +6,37 @@
 //
 
 import Foundation
+import _PhotosUI_SwiftUI
 
 class PostViewModel: ObservableObject{
     
     @Published var postText:String = ""
+    @Published var isLoading:Bool = false
+    @Published var uploadPost:Bool = false
+    
+    @Published var selectedItem: PhotosPickerItem? = nil
+    @Published var selectedImage: UIImage? = nil
+    
+    
+    func submitPost(completion: @escaping (Bool, String?) -> Void){
+        
+        isLoading = true
+        
+        let request = PostRequest(status: postText, image: selectedImage)
+        
+        PostService.shared.createPost(request: request) { success, message in
+            DispatchQueue.main.async {
+                self.isLoading = false
+                if success {
+                    self.uploadPost = true
+                    print("Successfully uploaded post \(String(describing: message))")
+                }else{
+                    print("Failed to upload post \(String(describing: message))")
+                }
+            }
+            
+            
+            
+        }
+    }
 }
