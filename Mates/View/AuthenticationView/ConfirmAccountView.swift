671ccfeaa1ddb1b98b30f743535a18749b6be1ed
Anurag Shrestha <anuragshrestha16@gmail.com>
2025-06-17 15:40:47 -0600

    handles confirming account if not during signin

--- Mates/View/AuthenticationView/ConfirmAccountView.swift
+++ Mates/View/AuthenticationView/ConfirmAccountView.swift
@@ -5,14 +5,103 @@
 //  Created by Anurag Shrestha on 6/17/25.
 //
 
+
+
 import SwiftUI
 
 struct ConfirmAccountView: View {
+    
+    
+    @StateObject var confirmVM = ConfirmAccountViewModel()
+    @State var showAlert: Bool = false
+    @State var alertMessage:String = ""
+    @State private var isLoading:Bool = false
+
+    var email: String
+    var password:String
+    
+    
     var body: some View {
-        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+        ZStack{
+            VStack {
+                InputField(text: $confirmVM.confirmationCode, placeholder: "Enter confirmation code")
+                
+                CustomButton(title: "Confirm") {
+                    
+                    //checks if the code is correct
+                    if confirmVM.confirmationCode.isEmpty {
+                        showAlert = true
+                        alertMessage = "Please enter a valid code"
+                    } else{
+                        isLoading = true
+                        confirmVM.confirmAccount{ success, message in
+                       
+                            if success{
+                                let signInRequest = SignInRequest(username: email, password: confirmVM.tempPassword)
+                                
+                                Task {
+                                    do {
+                                        let response = try await SignInService.shared.signInService(data: signInRequest)
+                                            isLoading = false
+                                        if response.success, let accessToken = response.accessToken {
+                                            KeychainHelper.saveAccessToken(accessToken)
+                                            confirmVM.isConfirmed = true
+                                        }else{
+                                            alertMessage = response.message ?? "Your account is now confirmed, but we couldn’t sign you in. Please sign in manually."
+                                            showAlert = true
+                                        }
+                                    }catch{
+                                        isLoading = false
+                                        alertMessage = error.localizedDescription
+                                        showAlert = true
+                                    }
+                                }
+                            } else{
+                                isLoading = false
+                                showAlert = true
+                                alertMessage = message ?? "Error occurred. \n Please signin again."
+                            }
+                        }
+                        
+                    }
+                }
+                .alert("Missing Code", isPresented: $showAlert){
+                    Button("Ok", role: .cancel) {}
+                } message: {
+                    Text(alertMessage)
+                }
+                .padding(.horizontal, 20)
+                .padding(.vertical, 10)
+            }
+            .onAppear{
+                confirmVM.email = email
+                confirmVM.tempPassword = password
+            }
+            .frame(maxWidth: .infinity, maxHeight: .infinity)
+            .background(Color.black)
+            .ignoresSafeArea()
+            .navigationBarBackButtonHidden(true)
+            .navigationBarHidden(true)
+            .navigationDestination(isPresented: $confirmVM.isConfirmed) {
+                MainView()
+            }
+            
+            //Shows Progress view until we get response from backend after sending the request
+            if isLoading {
+                Color.black.opacity(0.6)
+                    .ignoresSafeArea()
+                
+                ProgressView()
+                    .progressViewStyle(CircularProgressViewStyle(tint: .white))
+                    .scaleEffect(2)
+            }
+        }
     }
+    
 }
 
 #Preview {
-    ConfirmAccountView()
+    NavigationStack{
+        ConfirmView(email: "", password: "")
+    }
 }
--- Mates/ViewModel/AuthViewModel/ConfirmAccountViewModel.swift
+++ Mates/ViewModel/AuthViewModel/ConfirmAccountViewModel.swift
@@ -6,3 +6,38 @@
 //
 
 import Foundation
+
+
+class ConfirmAccountViewModel: ObservableObject {
+    
+    @Published var email:String = ""
+    @Published var tempPassword:String = ""
+    @Published var isConfirmed:Bool = false
+    @Published var errorMessage:String? = ""
+    @Published var confirmationCode:String = ""
+    
+    
+    
+    func confirmAccount(completion: @escaping (Bool, String?) -> Void) {
+        
+        print("email from view model: ", email)
+        let request = ConfirmAccountRequest(
+            username: email,
+            confirmationCode: confirmationCode
+        )
+        
+        Task{
+            do {
+                let response = try await ConfirmAccountService.shared.confirmAccount(data: request)
+                DispatchQueue.main.async {
+                    completion(response.success, response.message ?? response.error)
+                }
+            }catch{
+                DispatchQueue.main.async {
+                    completion(false, error.localizedDescription)
+                }
+            }
+        }
+        
+    }
+}
