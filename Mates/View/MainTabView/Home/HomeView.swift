82d81e45690ef1dd82eff2ccf0dc605a69480607
Anurag Shrestha <anuragshrestha16@gmail.com>
2025-06-17 20:19:11 -0600

    added header to Mates and around you, for you section

--- Mates/View/MainTabView/Home/HomeView.swift
+++ Mates/View/MainTabView/Home/HomeView.swift
@@ -23,6 +23,10 @@ struct Post: Identifiable {
 
 struct HomeView: View {
     
+    
+    @State private var lastoffset: CGFloat = 0
+    @State private var hideHeader: Bool = false
+    
     let posts: [Post] = [
         Post(name: "Ethan Carter", avatar: "ethan", time: "1d", text: "I'm looking for a study group for my Calculus class. Anyone interested?", imageName: "math", likes: 23, comments: 5, shares: 2),
         Post(name: "Sophia Clark", avatar: "sophia", time: "2d", text: "Just finished a great workout at the campus gym! Feeling energized and ready to tackle the day.", imageName: nil, likes: 18, comments: 3, shares: 1),
@@ -33,25 +37,92 @@ struct HomeView: View {
     ]
     
     var body: some View {
-        ZStack {
+        ZStack(alignment: .top) {
             Color.black.ignoresSafeArea()
             
-            ScrollView {
-                VStack(alignment: .leading) {
-                    ForEach(posts) { post in
-                        PostScreen(post:post)
-                            .padding(.bottom, 20)
-                            
-                          
+            ScrollViewReader{ proxy in
+                ScrollView {
+                    
+                    VStack(spacing: 0) {
+                        ForEach(posts.indices, id: \.self) { index in
+                            PostScreen(post:posts[index])
+                                .background(
+                                  index == 0 ?
+                                      GeometryReader { geo in
+                                        Color.clear
+                                          .preference(key: ScrollOffsetPreferenceKey.self,
+                                                      value: geo.frame(in: .named("scroll")).minY)
+                                  }
+                                   : nil
+                              )
+                              .padding(.bottom, 20)
+                        }
+                    }
+                    .padding(.top, 80)
+                }
+                .coordinateSpace(name:"scroll")
+                .onPreferenceChange(ScrollOffsetPreferenceKey.self){ newValue in
+                    let delta = newValue - lastoffset
+                    if abs(delta) > 1 {
+                        withAnimation{
+                            // hides the header if delta value is less then 0
+                            hideHeader = delta < 0
+                        }
+                        lastoffset = newValue
                     }
                 }
-               
+                
+            }
+            
+            if !hideHeader {
+                
+                VStack(spacing: 4) {
+                    Text("Mates")
+                        .font(.custom("Helvetica-Bold", size: 30))
+                        .italic()
+                        .foregroundColor(.white)
+                    
+                    HStack(spacing: 20) {
+                        Text("For you")
+                            .frame(maxWidth: .infinity, alignment: .center)
+                            .font(.customfont(.semibold, fontSize: 24))
+                        
+                        Spacer()
+                        
+                        Text("Around you")
+                            .frame(maxWidth: .infinity, alignment: .center)
+                            .font(.customfont(.semibold, fontSize: 24))
+                                  
+                    }
+                    .foregroundColor(.gray)
+            
+                    
+                }
+                .frame(maxWidth: .infinity)
+                .padding(.top, 10)
+                .padding(.bottom, 10)
+                .background(Color.black.opacity(0.95))
+                .transition(.move(edge: .top))
+                .animation(.easeInOut(duration: 0.25), value: hideHeader)
             }
         }
         .navigationBarBackButtonHidden(true)
         .navigationBarHidden(true)
     }
 }
+
+
+struct ScrollOffsetPreferenceKey: PreferenceKey {
+    static var defaultValue: CGFloat = 0
+    static func reduce(value: inout CGFloat, nextValue: () -> CGFloat){
+        value = nextValue()
+    }
+    
+    
+}
+
+
+
 #Preview {
     HomeView()
 }
