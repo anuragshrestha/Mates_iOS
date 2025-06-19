a42250aa4e889f7a00b839d2c129177f68bdfe91
Anurag Shrestha <anuragshrestha16@gmail.com>
2025-06-18 22:32:36 -0600

    calls the aroundyou api for posts

--- Mates/Service/Home/AroundYouService.swift
+++ Mates/Service/Home/AroundYouService.swift
@@ -6,3 +6,68 @@
 //
 
 import Foundation
+
+struct HomeFeedResponse: Decodable {
+    
+    let success: Bool
+    let user: [UserModel]
+    let posts: [PostModel]
+}
+
+enum AroundYouServiceError: Error {
+    
+    case unauthorized
+    case nodata
+    case invalidResponse(status:Int, error: String)
+    case urlError
+    case decodingError
+}
+
+class AroundYouService {
+    
+    static let shared = AroundYouService()
+    private init(){}
+    
+    func fetchHomeFeed() async throws -> HomeFeedResponse {
+        
+        
+        //checks if there is a access token
+        guard let accessToken = KeychainHelper.loadAccessToken() else {
+            throw AroundYouServiceError.unauthorized
+        }
+        
+        
+        //checks if its a valid url
+        guard let url = URL(string: "http://localhost:4000/aroundyou-feed") else {
+            throw AroundYouServiceError.urlError
+        }
+        
+        var request = URLRequest(url: url)
+        request.httpMethod = "GET"
+        request.setValue("Bearer \(accessToken)", forHTTPHeaderField: "Authorization")
+        
+        do {
+            let (data, response) = try await URLSession.shared.data(for: request)
+            
+            guard let httpResponse = response as? HTTPURLResponse else {
+                throw AroundYouServiceError.invalidResponse(status: -1, error: "No HTTP Response")
+            }
+            
+            if httpResponse.statusCode == 200 {
+                do {
+                    let decodedResponse = try JSONDecoder().decode(HomeFeedResponse.self, from: data)
+                         return decodedResponse
+                }catch {
+                    throw AroundYouServiceError.decodingError
+                }
+            }else{
+              
+                let error = (try? JSONDecoder().decode([String: String].self, from: data)["error"]) ?? "Unknow error"
+                throw AroundYouServiceError.invalidResponse(status: httpResponse.statusCode, error: error)
+            }
+        }catch {
+            throw error
+        }
+    
+    }
+}
