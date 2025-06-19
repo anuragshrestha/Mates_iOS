0d36d356858b6bb17ef08168f122e0c8ceeec6df
Anurag Shrestha <anuragshrestha16@gmail.com>
2025-06-18 22:33:37 -0600

    viewmodel for around screen

--- Mates/ViewModel/Home/AroundYou/AroundYouViewModel.swift
+++ Mates/ViewModel/Home/AroundYou/AroundYouViewModel.swift
@@ -6,3 +6,35 @@
 //
 
 import Foundation
+
+class AroundYouServiceViewModel: ObservableObject {
+    
+    @Published var isLoading:Bool = false
+    @Published var posts: [PostModel] = []
+    @Published var user: [UserModel] = []
+    @Published var errorMessage: String?
+    @Published var showError: Bool = false
+    
+    
+    func laodAroundYouFeed() async {
+        
+        isLoading = true
+        
+        defer{isLoading = false}
+        
+        do {
+            let data = try await AroundYouService.shared.fetchHomeFeed()
+            self.posts = data.posts
+            self.user = data.user
+            
+            if data.posts.isEmpty {
+                self.errorMessage = "No post"
+                self.showError = true
+            }
+            
+        }catch {
+            self.errorMessage = "Something went wrong. \n Please signin again."
+            self.showError = true
+        }
+    }
+}
