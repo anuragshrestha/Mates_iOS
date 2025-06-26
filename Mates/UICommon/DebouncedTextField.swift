1c138eb3102d88353c9efb4ba2e5ae3e2c6af525
Anurag Shrestha <anuragshrestha16@gmail.com>
2025-06-26 13:41:12 -0600

    reusable debounce for searching

---
+++ Mates/UICommon/DebouncedTextField.swift
@@ -0,0 +1,47 @@
+//
+//  DebouncedTextField.swift
+//  Mates
+//
+//  Created by Anurag Shrestha on 6/26/25.
+//
+
+import SwiftUI
+
+struct DebouncedTextField: View {
+    
+    let placeholder:String
+    @Binding var text:String
+    @State private var debounceTask: DispatchWorkItem?
+    var debounceDelay: TimeInterval = 0.5
+    var onDebounceChange: (String) -> Void
+    
+    
+    var body: some View {
+        
+        ZStack(alignment: .leading){
+            
+            if text.isEmpty {
+                Text(placeholder)
+                    .foregroundColor(.white.opacity(0.8))
+                    .padding(.horizontal, 5)
+            }
+            
+            TextField("", text: $text)
+                .onChange(of: text) { newValue in
+                    debounceTask?.cancel()
+                    
+                    let task = DispatchWorkItem{
+                        onDebounceChange(newValue)
+                    }
+                    debounceTask = task
+                    DispatchQueue.main.asyncAfter(deadline: .now() + debounceDelay, execute: task)
+                }
+        }
+    }
+}
+
+#Preview {
+    DebouncedTextField( placeholder: "Search...",
+                        text: .constant(""),
+                        onDebounceChange: { _ in })
+}
---
+++ Mates/View/UserProfile/UserProfileView.swift
@@ -0,0 +1,18 @@
+//
+//  UserProfileView.swift
+//  Mates
+//
+//  Created by Anurag Shrestha on 6/26/25.
+//
+
+import SwiftUI
+
+struct UserProfileView: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    UserProfileView()
+}
