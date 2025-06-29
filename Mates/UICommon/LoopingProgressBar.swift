dc6001b86ca9f8362523445f347874bfeb015b7d
Anurag Shrestha <anuragshrestha16@gmail.com>
2025-06-29 01:54:23 -0600

    progress bar

--- Mates/UICommon/LoopingProgressBar.swift
+++ Mates/UICommon/LoopingProgressBar.swift
@@ -8,10 +8,25 @@
 import SwiftUI
 
 struct LoopingProgressBar: View {
+    @State private var progress: Double = 0.0
+    let duration: TimeInterval = 2.0
+    
     var body: some View {
-        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+        ProgressView(value: progress)
+            .progressViewStyle(LinearProgressViewStyle(tint: .white.opacity(0.8)))
+            .scaleEffect(x: 1, y: 6, anchor: .center)
+            .padding(.horizontal)
+            .onAppear {
+                Timer.scheduledTimer(withTimeInterval: duration, repeats: true) { _ in
+                    progress = 0
+                    withAnimation(.linear(duration: duration)) {
+                        progress = 1.0
+                    }
+                }
+            }
     }
 }
+
 
 #Preview {
     LoopingProgressBar()
