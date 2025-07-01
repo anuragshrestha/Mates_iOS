4121977bb777a18e3883ccfce0c280513a524b17
Anurag Shrestha <anuragshrestha16@gmail.com>
2025-07-01 00:57:50 -0600

    model for user post

---
+++ Mates/View/MainTabView/Account/PostDetailView.swift
@@ -0,0 +1,18 @@
+//
+//  PostDetailView.swift
+//  Mates
+//
+//  Created by Anurag Shrestha on 6/30/25.
+//
+
+import SwiftUI
+
+struct PostDetailView: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    PostDetailView()
+}
