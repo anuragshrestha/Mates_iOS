92c4eefe1f887672d0c220dbc7db2be0e24d4427
Anurag Shrestha <anuragshrestha16@gmail.com>
2025-07-08 23:44:40 -0600

    view for forgot password in account setting

---
+++ Mates/View/MainTabView/Account/AccountSettings/Settings/ForgotPassword.swift
@@ -0,0 +1,109 @@
+//
+//  ForgotPassword.swift
+//  Mates
+//
+//  Created by Anurag Shrestha on 7/8/25.
+//
+
+import SwiftUI
+
+struct ForgotPassword: View {
+    
+    @StateObject var forgotVM = ForgotPasswordViewModel()
+    @State var showAlert:Bool = false
+    @State var alertMessage:String = ""
+    @State private var showSuccessAlert:Bool = false
+    @State private var isLoading:Bool = false
+    
+    
+    var body: some View {
+        
+        ZStack{
+            
+            VStack{
+                
+                InputField(text: $forgotVM.confirmationCode, placeholder: "Enter confirmation code")
+                    .padding(.bottom, 10)
+                
+                
+                SecureTextField(password: $forgotVM.newPassword, placeholder: "Enter new password", isSecure: $forgotVM.isSecure)
+                    .padding(.bottom, 10)
+                
+                SecureTextField(password: $forgotVM.confirmNewPassword, placeholder: "Confirm the password", isSecure: $forgotVM.isSecure2)
+                    .padding(.bottom, 10)
+                
+                
+                CustomButton(title: "Reset Password") {
+                    if forgotVM.confirmationCode.isEmpty {
+                        alertMessage = "Enter confirmation code"
+                        showAlert = true
+                    } else if forgotVM.newPassword.isEmpty {
+                        showAlert = true
+                        alertMessage = "Enter the new password"
+                    } else if forgotVM.confirmNewPassword.isEmpty{
+                        showAlert = true
+                        alertMessage = "Enter the confirm password"
+                    } else if !forgotVM.isValidPassword(forgotVM.newPassword) || !forgotVM.isValidPassword(forgotVM.confirmNewPassword){
+                        alertMessage = "Password must be at least 6 characters and have \n one upper, lower, digit and special character"
+                        showAlert = true
+                    } else if !forgotVM.checkPassword(password: forgotVM.newPassword, confirmPassword: forgotVM.confirmNewPassword){
+                        alertMessage = "Confirm password doesnot match with new password"
+                        showAlert = true
+                    } else {
+                        isLoading = true
+                        forgotVM.confirmForgotPassword { success, message in
+                            isLoading = false
+                            if success{
+                                showSuccessAlert = true
+                            }else{
+                                alertMessage = message ?? "Failed to reset password"
+                                showAlert = true
+                            }
+                        }
+                    }
+                }
+                .alert("Error", isPresented: $showAlert) {
+                    Button("Ok", role: .cancel){}
+                }message: {
+                    Text(alertMessage)
+                }
+                .padding(.horizontal, 20)
+                .alert("Success", isPresented: $showSuccessAlert) {
+                    Button("Ok"){
+                        forgotVM.isConfirmedForgotPassword = true
+                    }
+                }message: {
+                    Text("Successfully reset password")
+                }
+                
+                Spacer()
+            }
+            .padding(.top, 200)
+            .frame(maxWidth: .infinity, maxHeight: .infinity)
+            .background(Color.black)
+            .ignoresSafeArea()
+      
+            
+            //Shows Progress view until we get response from backend after sending the request
+            if isLoading {
+                
+                Color.black.opacity(0.6)
+                    .ignoresSafeArea()
+                
+                ProgressView()
+                    .progressViewStyle(CircularProgressViewStyle(tint: .white))
+                    .scaleEffect(2)
+            }
+        }
+        
+    }
+}
+
+
+
+#Preview {
+    
+    NavigationStack {
+        ForgotPassword()
+    }
+}
