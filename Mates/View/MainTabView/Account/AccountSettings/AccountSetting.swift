1986762cb31f7939f68275fe989641355b0465a4
Anurag Shrestha <anuragshrestha16@gmail.com>
2025-07-09 22:50:58 -0600

    navigtaes to forgot password screen and passes the view model

--- Mates/View/MainTabView/Account/AccountSettings/AccountSetting.swift
+++ Mates/View/MainTabView/Account/AccountSettings/AccountSetting.swift
@@ -16,188 +16,270 @@ struct AccountSetting: View {
      * feedback
      *help & support*
      */
-    
+    @Environment(\.dismiss) private var dismiss
     @StateObject var signOutVM = SignOutViewModel()
     @AppStorage("isSignedIn") var isSignedIn:Bool = false
     @State var showAlert:Bool = false
     @State var alertMessage:String = ""
     @State private var isLoading:Bool = false
+    @State private var path = NavigationPath()
+    @StateObject var forgotPasswordVM = ForgotPasswordViewModel()
     
     var user : UserAccountModel?
     
     
+    
+    enum SettingsRoute: Hashable{
+        case forgotPassword
+    }
+    
     var body: some View {
-       
-        ZStack(alignment: .leading){
+        
+        NavigationStack(path: $path){
             
-            Color.black.background().ignoresSafeArea()
-            
-            VStack(alignment: .leading, spacing: 6) {
+            ZStack(alignment: .leading){
                 
-                //shows account settings
-                HStack(spacing: 8) {
+                Color.black.background().ignoresSafeArea()
+                
+                VStack(alignment: .leading, spacing: 6) {
                     
-                    Button(action:{
-                        print("pressed")
-                    }){
-                        Image(systemName: "gearshape")
-                            .font(.system(size: 22, weight: .semibold))
-                            .foregroundColor(.gray)
-                            .frame(width: 30, alignment: .leading)
-                           
-                           
+                    //shows account settings
+                    HStack(spacing: 8) {
                         
-                        Text("Accounts Settings")
-                            .font(.system(size: 26, weight: .semibold))
-                            .foregroundColor(.white)
-                            .padding(.leading, 5)
-                        
-                        Spacer()
-                        
-                        Image(systemName: "chevron.right")
-                            .font(.system(size: 20))
-                            .foregroundColor(.gray)
-                    }
-                }
-                .padding(.bottom, 10)
-                
-                
-                //options for editing profile
-                HStack(spacing: 6) {
-                    
-                    Button(action: {
-                        
-                    }){
-                        Image(systemName: "pencil")
-                            .font(.system(size: 22, weight: .semibold))
-                            .foregroundColor(.gray)
-                            .frame(width: 30, alignment: .leading)
-                        
-                        
-                        Text("Edit Profile")
-                            .font(.system(size: 26, weight: .semibold))
-                            .foregroundColor(.white)
-                            .padding(.leading, 5)
-                        
-                        Spacer()
-                        
-                        Image(systemName: "chevron.right")
-                            .font(.system(size: 20))
-                            .foregroundColor(.gray)
-                    }
-                }
-                .padding(.bottom, 10)
-                
-                
-                //feedback option
-                HStack(spacing: 6) {
-                    
-                    Button(action: {
-                        
-                    }){
-                        Image(systemName: "envelope")
-                            .font(.system(size: 22, weight: .semibold))
-                            .foregroundColor(.gray)
-                            .frame(width: 30, alignment: .leading)
-                        
-                        Text("Feedback")
-                            .font(.system(size: 26, weight: .semibold))
-                            .foregroundColor(.white)
-                            .padding(.leading, 5)
-                        
-                        Spacer()
-                        
-                        Image(systemName: "chevron.right")
-                            .font(.system(size: 20))
-                            .foregroundColor(.gray)
-                    }
-                }
-                .padding(.bottom, 10)
-                
-                
-                //option for help and support
-                HStack(spacing: 6) {
-                    
-                    Button(action: {
-                        
-                    }){
-                        Image(systemName: "questionmark.circle")
-                            .font(.system(size: 22, weight: .semibold))
-                            .foregroundColor(.gray)
-                            .frame(width: 30, alignment: .leading)
-                        
-                        Text("Help & Support")
-                            .font(.system(size: 26, weight: .semibold))
-                            .foregroundColor(.white)
-                            .padding(.leading, 5)
-                        
-                        Spacer()
-                        
-                        Image(systemName: "chevron.right")
-                            .font(.system(size: 20))
-                            .foregroundColor(.gray)
-                    }
-                }
-                .padding(.bottom, 10)
-                
-                
-                Spacer()
-                
-                
-            }
-            .padding(.horizontal)
-            .padding(.top, 50)
-       
-            VStack{
-                Spacer()
-                
-                
-                HStack(){
-                    
-                    Spacer()
-                    
-                    Button(action: {
-                        print("pressed signout button")
-                        isLoading = true
-                        signOutVM.signOut { success, message in
-                            isLoading = false
-                            if success{
-                                print("successfully logout.")
-    //                            KeychainHelper.deleteAccessToken()
-    //                            isSignedIn = false
-                            }else{
-                                alertMessage = "Failed to Sign Out. \n Please try again later."
-                                showAlert = true
-                                print(message ?? "failed to log out")
+                        Button(action:{
+                            
+                            //clears all the data if exits
+                            forgotPasswordVM.confirmationCode = ""
+                            forgotPasswordVM.newPassword = ""
+                            forgotPasswordVM.confirmNewPassword = ""
+                            forgotPasswordVM.isConfirmedForgotPassword = false
+                            
+                            forgotPasswordVM.email = user?.email ?? ""
+                            isLoading = true
+                            forgotPasswordVM.forgotPassword { success, message in
+                                isLoading = false
+                                
+                                if success{
+                                    forgotPasswordVM.email = user?.email ?? ""
+                                    path.append(SettingsRoute.forgotPassword)
+                                }else{
+                                    showAlert = true
+                                    alertMessage = "Failed to send code"
+                                }
                             }
-                        }
-                    }) {
-                        Text("Log out")
-                            .font(.system(size: 26, weight: .semibold))
-                            .foregroundColor(.white)
-                            .padding(.horizontal, 40)
-                            .padding(.vertical, 10)
-                            .background(Color.red)
-                            .cornerRadius(14)
                            
+                        }){
+                            Image(systemName: "gearshape")
+                                .font(.system(size: 22, weight: .semibold))
+                                .foregroundColor(.gray)
+                                .frame(width: 30, alignment: .leading)
+                            
+                            
+                            
+                            Text("Forgot Password")
+                                .font(.system(size: 26, weight: .semibold))
+                                .foregroundColor(.white)
+                                .padding(.leading, 5)
+                            
+                            Spacer()
+                            
+                            Image(systemName: "chevron.right")
+                                .font(.system(size: 20))
+                                .foregroundColor(.gray)
+                        }
                     }
-                    .alert("Error", isPresented: $showAlert){
-                        Button("Ok", role: .cancel){}
-                    } message: {
-                        Text(alertMessage)
+                    .padding(.bottom, 10)
+                    
+                    
+                    
+                    //changes the password
+                    HStack(spacing: 8) {
+                        
+                        Button(action:{
+                            print("pressed")
+                        }){
+                            Image(systemName: "gearshape")
+                                .font(.system(size: 22, weight: .semibold))
+                                .foregroundColor(.gray)
+                                .frame(width: 30, alignment: .leading)
+                            
+                            
+                            
+                            Text("Change Password")
+                                .font(.system(size: 26, weight: .semibold))
+                                .foregroundColor(.white)
+                                .padding(.leading, 5)
+                            
+                            Spacer()
+                            
+                            Image(systemName: "chevron.right")
+                                .font(.system(size: 20))
+                                .foregroundColor(.gray)
+                        }
                     }
-
+                    .padding(.bottom, 10)
+                    
+                    
+                    
+                    //options for editing profile
+                    HStack(spacing: 6) {
+                        
+                        Button(action: {
+                            
+                        }){
+                            Image(systemName: "pencil")
+                                .font(.system(size: 22, weight: .semibold))
+                                .foregroundColor(.gray)
+                                .frame(width: 30, alignment: .leading)
+                            
+                            
+                            Text("Edit Profile")
+                                .font(.system(size: 26, weight: .semibold))
+                                .foregroundColor(.white)
+                                .padding(.leading, 5)
+                            
+                            Spacer()
+                            
+                            Image(systemName: "chevron.right")
+                                .font(.system(size: 20))
+                                .foregroundColor(.gray)
+                        }
+                    }
+                    .padding(.bottom, 10)
+                    
+                    
+                    //feedback option
+                    HStack(spacing: 6) {
+                        
+                        Button(action: {
+                            
+                        }){
+                            Image(systemName: "envelope")
+                                .font(.system(size: 22, weight: .semibold))
+                                .foregroundColor(.gray)
+                                .frame(width: 30, alignment: .leading)
+                            
+                            Text("Feedback")
+                                .font(.system(size: 26, weight: .semibold))
+                                .foregroundColor(.white)
+                                .padding(.leading, 5)
+                            
+                            Spacer()
+                            
+                            Image(systemName: "chevron.right")
+                                .font(.system(size: 20))
+                                .foregroundColor(.gray)
+                        }
+                    }
+                    .padding(.bottom, 10)
+                    
+                    
+                    //option for help and support
+                    HStack(spacing: 6) {
+                        
+                        Button(action: {
+                            
+                        }){
+                            Image(systemName: "questionmark.circle")
+                                .font(.system(size: 22, weight: .semibold))
+                                .foregroundColor(.gray)
+                                .frame(width: 30, alignment: .leading)
+                            
+                            Text("Help & Support")
+                                .font(.system(size: 26, weight: .semibold))
+                                .foregroundColor(.white)
+                                .padding(.leading, 5)
+                            
+                            Spacer()
+                            
+                            Image(systemName: "chevron.right")
+                                .font(.system(size: 20))
+                                .foregroundColor(.gray)
+                        }
+                    }
+                    .padding(.bottom, 10)
+                    
                     
                     Spacer()
+                    
+                    
                 }
-                .padding(.bottom, 30)
-              
+                .padding(.horizontal)
+                .padding(.top, 50)
+                
+                VStack{
+                    Spacer()
+                    
+                    
+                    HStack(){
+                        
+                        Spacer()
+                        
+                        Button(action: {
+                            print("pressed signout button")
+                            isLoading = true
+                            signOutVM.signOut { success, message in
+                                isLoading = false
+                                if success{
+                                    print("successfully logout.")
+                                    //                            KeychainHelper.deleteAccessToken()
+                                    //                            isSignedIn = false
+                                }else{
+                                    alertMessage = "Failed to Sign Out. \n Please try again later."
+                                    showAlert = true
+                                    print(message ?? "failed to log out")
+                                }
+                            }
+                        }) {
+                            Text("Log out")
+                                .font(.system(size: 26, weight: .semibold))
+                                .foregroundColor(.white)
+                                .padding(.horizontal, 40)
+                                .padding(.vertical, 10)
+                                .background(Color.red)
+                                .cornerRadius(14)
+                            
+                        }
+                        .alert("Error", isPresented: $showAlert){
+                            Button("Ok", role: .cancel){}
+                        } message: {
+                            Text(alertMessage)
+                        }
+                        
+                        
+                        Spacer()
+                    }
+                    .padding(.bottom, 30)
+                    
+                }
+                
+                
+                //Shows Progress view until we get response from backend after sending the request
+                if isLoading {
+                    Color.black.opacity(0.4)
+                        .ignoresSafeArea()
+                    
+                    ProgressView()
+                        .progressViewStyle(CircularProgressViewStyle(tint: .white))
+                        .scaleEffect(2)
+                }
+                
             }
-           
-            
+            .navigationTitle("Settings and activity")
+            .navigationBarBackButtonHidden(false)
+            .navigationBarTitleDisplayMode(.inline)
+            .toolbar {
+                ToolbarItem(placement: .principal) {
+                    EmptyView()
+                }
+            }
+            .navigationDestination(for: SettingsRoute.self) { route in
+                switch route {
+                case .forgotPassword:
+                    ForgotPassword(forgotVM: forgotPasswordVM)
+                }
+            }
         }
-        .navigationTitle("Settings and activity")
-        .navigationBarBackButtonHidden(true)
     }
 }
 
