dfd74fe0552a3c0a032a79f92d8b413724cd696c
Anurag Shrestha <anuragshrestha16@gmail.com>
2025-07-13 00:11:53 -0600

    edit profile view

--- Mates/View/MainTabView/Account/AccountSettings/Settings/EditProfileView.swift
+++ Mates/View/MainTabView/Account/AccountSettings/Settings/EditProfileView.swift
@@ -8,11 +8,65 @@
 import SwiftUI
 
 struct EditProfileView: View {
+    
+   @Binding var user: UserAccountModel
+  
+    @State private var searchText: String = ""
+    @State private var showMajorPicker: Bool = false
+    @State private var showYearPicker: Bool = false
+    
+   
+    
     var body: some View {
-        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+       
+        ZStack{
+            Color.black.background().ignoresSafeArea()
+            
+            VStack{
+                
+                InputField(text: $user.fullName, placeholder: "Enter your full Name")
+                    .padding(.bottom, 10)
+                
+                DropdownField(title: "Select your major", value: user.major) {
+                    searchText = ""
+                    showMajorPicker.toggle()
+                }
+                
+                DropdownField(title: "Select your school year", value: user.schoolYear) {
+                    searchText = ""
+                    showYearPicker.toggle()
+                }
+                
+            }
+        }
     }
 }
 
+
+
+
 #Preview {
-    EditProfileView()
+
+    struct PreviewWrapper: View {
+    
+        @State var user = UserAccountModel(
+            id: UUID(),
+            email: "test@example.com",
+            fullName: "Anurag Shrestha",
+            universityName: "Harvard University",
+            major: "Computer Science",
+            schoolYear: "Senior",
+            createdAt: "2024-01-01T00:00:00Z",
+            profileImageUrl: "https://example.com/profile.jpg",
+            postCount: 5,
+            followersCount: 100,
+            followingCount: 80
+        )
+
+        var body: some View {
+            EditProfileView(user: $user)
+        }
+    }
+
+    return PreviewWrapper()
 }
