9d12f592154d7c493da3437355b3c83194ecfa27
Anurag Shrestha <anuragshrestha16@gmail.com>
2025-07-18 23:54:59 -0600

    custom field for entering the help and support message

---
+++ Mates/UICommon/HelpSupportField.swift
@@ -0,0 +1,70 @@
+//
+//  HelpSupportField.swift
+//  Mates
+//
+//  Created by Anurag Shrestha on 7/18/25.
+//
+
+import SwiftUI
+
+struct HelpSupportField: View {
+
+
+        @Binding var text: String
+        var placeholder: String
+        
+        @FocusState private var isFocused: Bool
+
+        var body: some View {
+            ZStack(alignment: .topLeading) {
+                      
+                      RoundedRectangle(cornerRadius: 12)
+                    .fill(Color.white.opacity(0.8))
+                 
+                      // Multiline text input
+                      TextEditor(text: $text)
+                         .focused($isFocused)
+                         .foregroundColor(.black.opacity(0.8))
+                         .font(.customfont(.semibold, fontSize: 20))
+                         .frame(height: 130)
+                         .padding(.horizontal, 10)
+                         .padding(.vertical, 12)
+                         .disableAutocorrection(true)
+                         .textInputAutocapitalization(.never)
+                         .scrollContentBackground(.hidden)
+                         .clipShape(RoundedRectangle(cornerRadius: 12))
+                      
+                      // Placeholder if text is empty and not focused on the TextEditor
+                      if text.isEmpty && !isFocused {
+                          Text(placeholder)
+                              .foregroundColor(.black.opacity(0.8))
+                              .font(.customfont(.semibold, fontSize: 20))
+                              .padding(.top, 18)
+                              .padding(.horizontal, 16)
+                              .allowsHitTesting(false)
+                      }
+              }
+              .frame(height: 130)
+              .padding(.horizontal, 6)
+        
+     
+           
+        }
+    }
+
+
+
+
+
+
+#Preview {
+    HelpSupportField_Preview()
+}
+
+struct HelpSupportField_Preview: View {
+    @State private var text: String = ""
+    
+    var body: some View {
+        HelpSupportField(text: $text, placeholder: "What's on your mind?")
+    }
+}
