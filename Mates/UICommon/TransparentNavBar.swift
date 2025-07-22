53889bf531557f0d6cc14ceb4e980871f2909301
Anurag Shrestha <anuragshrestha16@gmail.com>
2025-07-22 11:50:15 -0600

    deleted the duplicate Account View

---
+++ Mates/UICommon/TransparentNavBar.swift
@@ -0,0 +1,18 @@
+//
+//  TransparentNavBar.swift
+//  Mates
+//
+//  Created by Anurag Shrestha on 7/22/25.
+//
+
+import SwiftUI
+
+struct TransparentNavBar: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    TransparentNavBar()
+}
