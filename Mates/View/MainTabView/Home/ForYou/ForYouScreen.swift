4ed471df8561624fc9a13fd084e6c6740251ba14
Anurag Shrestha <anuragshrestha16@gmail.com>
2025-08-26 23:29:16 -0600

    independent screen for both Around you and ForYou

--- Mates/View/MainTabView/Home/ForYou/ForYouScreen.swift
+++ Mates/View/MainTabView/Home/ForYou/ForYouScreen.swift
@@ -8,11 +8,8 @@
 import SwiftUI
 
 struct ForYouScreen: View {
-    var body: some View {
-        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
-    }
-}
-
-#Preview {
-    ForYouScreen()
+    @ObservedObject var vm: FeedViewModel
+    
+    
+    var body: some View { FeedListScreen(vm: vm) }
 }
